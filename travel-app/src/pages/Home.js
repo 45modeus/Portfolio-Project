5832ee86461ed112785a8e119f83910084c73a60
45modeus <lauriemichel66@gmail.com>
2026-04-13 13:14:50 +0400

    first commit - cleaned everything

---
+++ travel-app/src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Island Escape Travel</h1>
+      <p>Your dream vacation starts here 🌴</p>
+    </div>
+  );
+}
+
+export default Home;
