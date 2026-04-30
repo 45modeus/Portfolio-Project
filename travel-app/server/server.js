57522ab3981b21a09f3031db629c2d3da1543511
45modeus <lauriemichel66@gmail.com>
2026-04-30 10:50:15 +0400

    changed the app.listen to match render

--- travel-app/server/server.js
+++ travel-app/server/server.js
@@ -126,6 +126,8 @@ app.delete("/messages/:id", verifyToken, async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
