dc1607b58946adc9c3e6ad2ac5304c440d42b5ec
45modeus <lauriemichel66@gmail.com>
2026-05-01 10:22:15 +0400

    fixing cors for admin

--- travel-app/server/server.js
+++ travel-app/server/server.js
@@ -10,10 +10,8 @@ const app = express();
 
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/", (req, res) => {
+  res.json({ message: "Backend running ✅" });
 });
 
 /* -----------------------------
