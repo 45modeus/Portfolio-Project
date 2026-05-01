74d4a5089851eb58ba269b44ee47fc1e3dd1bbea
45modeus <lauriemichel66@gmail.com>
2026-05-01 10:16:37 +0400

    fixing cors for admin

--- travel-app/server/server.js
+++ travel-app/server/server.js
@@ -8,6 +8,14 @@ require("dotenv").config();
 
 const app = express();
 
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 /* -----------------------------
    ADMIN CONFIG
 ----------------------------- */
